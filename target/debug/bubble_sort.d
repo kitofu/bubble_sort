5e07c0ad568ffbe8d80acdbd93e8bb8b34f24c83
Satoshi Kito <kitos@kawai.co.jp>
2022-03-02 13:27:53 +0900

    first

---
+++ target/debug/bubble_sort.d
@@ -0,0 +1,1 @@
+C:\Users\317292\projects\bubble_sort\target\debug\bubble_sort.exe: C:\Users\317292\projects\bubble_sort\src\main.rs
---
+++ target/debug/deps/bubble_sort.d
@@ -0,0 +1,5 @@
+C:\Users\317292\projects\bubble_sort\target\debug\deps\bubble_sort.exe: src\main.rs
+
+C:\Users\317292\projects\bubble_sort\target\debug\deps\bubble_sort.d: src\main.rs
+
+src\main.rs:
