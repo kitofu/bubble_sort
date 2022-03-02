db58d31df70e823f552e0a10f2327526e1e2c55a
Satoshi Kito <kitos@kawai.co.jp>
2022-03-02 14:23:51 +0900

    add:いけたっぽい

--- src/main.rs
+++ src/main.rs
@@ -17,13 +17,28 @@ use rand::Rng;
 fn main() {
     println!("This is bubble sort!");
 
-    let mut a: [i32; 101] = [1; 101];
+    //配列を用意するi
+    const ARRAY_NUM:usize = 100;
+    let mut a: [i32; ARRAY_NUM] = [1; ARRAY_NUM];
 
-    for n in 1..101{
+    for n in 0..ARRAY_NUM{
         let rand = rand::thread_rng().gen_range(-1000, 1001);
         a[n] = rand;
         println!("n{} = {}", n, a[n]);
     }
-
+    //アルゴリズム実行する
+    for i in 0..ARRAY_NUM - 1{
+        for j in 0..ARRAY_NUM-1-i{
+            if( a[j] > a[j+1] ) {
+                let big_num = a[j];
+                a[j] = a[j+1];
+                a[j+1] = big_num;
+            }
+        }
+    }
+    //Sort shiowatta
+    for n in 0..ARRAY_NUM{
+            println!("Sort shimashita a_{} = {}", n, a[n]);
+        }
 
 }
